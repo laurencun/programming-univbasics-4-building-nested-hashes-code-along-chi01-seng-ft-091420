d81485020dd3268c6e61cd90e326a8492ef70465
Lauren Cunningham <lauren.busbee@gmail.com>
2020-07-05 22:25:35 +0000

    Automatically backed up by Learn

--- lib/03_third_challenge.rb
+++ lib/03_third_challenge.rb
@@ -26,7 +26,7 @@ def third_challenge
       },
       :matriarch => {
         #your key/value pairs here
-        :name => "Lady Capulet"
+        :name => "Lady Capulet",
         :age => "51"
       },
       :heroine => {
