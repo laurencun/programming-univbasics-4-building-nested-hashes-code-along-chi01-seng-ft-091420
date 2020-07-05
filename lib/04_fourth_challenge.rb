638d2e593a6e588c7c6a9a72aed270851273650b
Lauren Cunningham <lauren.busbee@gmail.com>
2020-07-05 22:30:01 +0000

    Automatically backed up by Learn

--- lib/04_fourth_challenge.rb
+++ lib/04_fourth_challenge.rb
@@ -22,9 +22,9 @@ def fourth_challenge
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
         #your hashes here!
-         {:name => "Benvolio",
-          :age => "17", 
-          :attitude => "worried"
+         {:name => "Steven",
+          :age => "30", 
+          :attitude => "confused"
         },
         {:name => "Mercutio",
           :age => "18", 
